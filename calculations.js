b1d2f5bfe9c679e356d0449f13668eeb8eb3ff2a
Bartollo12 <bartek.wieczorek2012@gmail.com>
2026-08-01 00:37:48 +0200

    added the website

---
+++ calculations.js
@@ -0,0 +1,29 @@
+let dinodoomz = null
+let dinodoomzYes = document.getElementById("dinodoomz-yes")
+let dinodoomzNo = document.getElementById("dinodoomz-no")
+let dinodoomzAnswer = document.getElementById("dinodoomz-answer")
+
+let finishBtn = document.getElementById("finish-btn")
+let ifNotPressedText = document.getElementById("if-not-pressed-text")
+
+dinodoomzYes.addEventListener("click", function() {
+    dinodoomz = true
+    dinodoomzAnswer.textContent = "Yes"
+});
+
+dinodoomzNo.addEventListener("click", function() {
+    dinodoomz = false
+    dinodoomzAnswer.textContent = "No"
+});
+
+finishBtn.addEventListener("click", function() {
+    if (dinodoomz == true) {
+        location.href = "isDino.html";
+    }
+    else if (dinodoomz == false) {
+        location.href = "isNotDino.html";
+    }
+    else {
+        ifNotPressedText.textContent = "bro u have to like... answer...";
+    }
+});
